1b95772d9291ac48b1c7fa28bfa76648793b4116
wail-10 <koundiwail02@gmail.com>
2024-06-01 15:58:29 +0000

    create left side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <div>
-            <main>
-                <p className="text-white-1">LEFT SIDEBAR</p>
+        <div className="relative flex flex-col">
+            <main className="relative flex bg-black-3">
+                <LeftSidebar />
+                <section></section>
                 {children}
                 <p className="text-white-1">RIGHT SIDEBAR</p>
             </main>
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { usePathname, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+const LeftSidebar = () => {
+    const pathName = usePathname()
+    const router = useRouter()
+
+    return (
+        <section className='left_sidebar'>
+            <nav className='flex flex-col gap-6'>
+                <Link href='/' className='flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center'>
+                    <Image src="/icons/logo.svg" alt='logo' width={23} height={27} />
+                    <h1 className='text-24 font-extrabold text-white-1 max-lg:hidden'>PodSora</h1>
+                </Link>
+                {sidebarLinks.map(({route, label, imgURL}) => {
+                    const isActive = pathName === route || pathName.startsWith(`${route}/`)
+
+                    return <Link 
+                    href={route} 
+                    key={label} 
+                    className={cn(
+                        'flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start',
+                        {
+                            'bg-nav-focus border-r-4 border-orange-1': isActive
+                        }
+                        )}>
+                        <Image src={imgURL} alt={label} width={24} height={24} />
+                        <p>{label}</p>                        
+                    </Link>
+                })}
+            </nav>
+        </section>
+    )
+}
+
+export default LeftSidebar
