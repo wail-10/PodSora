2fe737610b0f08173e36636b0e85aa29400e56fe
wail-10 <koundiwail02@gmail.com>
2024-06-02 10:05:58 +0000

    create home page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { podcastData } from '@/constants'
+import PodcastCard from '@/components/PodcastCard'
 
 const Home = () => {
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
-        {/* <Button className='text-white-1 bg-orange-1'>Button</Button> */}
+        <div className='podcast_grid'>
+          {podcastData.map(({id, imgURL, description, title}) => (
+            <PodcastCard key={id} 
+            imgUrl={imgURL} 
+            description={description} 
+            title={title}
+            podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   )
